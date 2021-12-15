e47309a2f508595d81019227f4987902c85bfc5d
Raphaël Huchet <raph@selego.co>
2021-12-15 23:44:39 +0100

    fix: fix ci

--- test/index.test.ts
+++ test/index.test.ts
@@ -2,27 +2,22 @@ import { importCsv } from "../src/index";
 import { Client } from "@elastic/elasticsearch";
 import * as fs from "fs";
 
-jest.mock("@elastic/elasticsearch", () => ({
-  Client: jest.fn().mockImplementation(() => {
-    return {
-      bulk: jest.fn().mockImplementation(async ({ body }) => {
-        global.ESMOCK_totalItemsCount = body.length / 2;
-        return {
-          body: {
-            errors: 0,
-          },
-        };
-      }),
-      count: jest.fn().mockImplementation(async ({ index }) => {
-        return {
-          body: {
-            count: global.ESMOCK_totalItemsCount,
-          },
-        };
-      }),
-    };
-  }),
-}));
+// Elasticsearch is not available on CI.
+if (process.env.CI) {
+  jest.mock("@elastic/elasticsearch", () => ({
+    Client: jest.fn().mockImplementation(() => {
+      return {
+        bulk: jest.fn().mockImplementation(async ({ body }) => {
+          global.ESMOCK_totalItemsCount = body.length / 2;
+          return { body: { errors: 0 } };
+        }),
+        count: jest.fn().mockImplementation(async ({ index }) => {
+          return { body: { count: global.ESMOCK_totalItemsCount } };
+        }),
+      };
+    }),
+  }));
+}
 
 test("importCsv", async () => {
   fs.writeFileSync(
